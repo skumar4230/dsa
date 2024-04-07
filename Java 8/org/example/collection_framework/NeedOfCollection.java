9ef7575d0c0214356c7a9e4aad8e65d663712fa3
sourav-code <skumar4230@gmail.com>
2024-04-07 18:23:27 +0530

    need of collection

---
+++ Java 8/org/example/collection_framework/NeedOfCollection.java
@@ -0,0 +1,28 @@
+package org.example.collection_framework;
+
+public class NeedOfCollection {
+    public static void main(String[] args) {
+        //initially if we need to store n integer numbers then we have to declare n variables
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 30;
+        int e = 30;
+        int f = 30;
+        int g = 30;
+        int h = 30;
+        int i = 30;
+        //to tackle this situation we have arrays
+        int[] arrsy = new int[10000];
+        //we can store multiple values inside a single variable array
+        //limitation of array
+        //1. Arrays are fixed in size
+        //2.can hold only homogeneous data elements
+        //3. we have to provide index value to add some data
+        //4. ready made APIs support is not available in array
+        //there comes collection which have prebuilt functions to work with data structures
+
+
+
+    }
+}
